5953528b0b2a1ad5c6ab258b338c940aa97c7e37
Iris <98936068+6iris6@users.noreply.github.com>
2022-06-24 17:45:35 -0700

    update adpp in scda.2022 (#46)

--- data_raw/rcd.R
+++ data_raw/rcd.R
@@ -4,7 +4,7 @@ releases <- c(
   # nolint start
   # "2022_01_28" = "7f2200332aab693d2ef4149a0f67b7708a2db516",
   # nolint end
-  "2022_02_28" = "fc3620365f38c36e45880cbdebfe16508d902fa6"
+  "2022_02_28" = "f30ede11f803a8bd4cf6872410dfc2a09e13961f"
 )
 
 # https://stackoverflow.com/questions/5577221/
