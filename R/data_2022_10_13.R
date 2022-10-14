1469978dfea80ceb5e2dbd539bee58bd68a1647d
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2022-10-14 12:07:46 -0400

    Update datasets

---
+++ R/data_2022_10_13.R
@@ -0,0 +1,62 @@
+#' Cached datasets from the 2022_10_13 release of `random.cdisc.data`
+#'
+#' @name rcd_data_2022_10_13
+#'
+NULL
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adsl"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adab"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adae"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adaette"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adcm"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_addv"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adeg"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adex"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adhy"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adlb"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_admh"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adpc"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adpp"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adqs"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adrs"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adsub"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adtr"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_adtte"
+
+#' @rdname rcd_data_2022_10_13
+"rcd_2022_10_13_advs"
